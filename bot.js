07c86b5d3de8cff28dbb96b37def237f98eb4bc3
danomov <dx555r@gmail.com>
2020-06-27 12:51:37 +0400

    changes

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 let Discord = require("discord.io");
 let logger = require("winston");
 const fetch = require("node-fetch");
-const token = "NzI2MDgzMTI2NjAxNzExNzI5.XvcGbw.Tu9-t7HLpw2m5eEyHqFjZXzDeH8";
+const token = process.env.TOKEN;
 
 function configer() {
   logger.remove(logger.transports.Console);
